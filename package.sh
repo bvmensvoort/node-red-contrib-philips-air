bb07e90fcf8dc79f45cd5e73c70173f1926db9eb
bvmensvoort <info@bvmensvoort.nl>
2020-09-03 19:46:06 +0200

    Update GitHub release flow for easy publishing

---
+++ package.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "-- Resetting environment"
+rm -rf node_modules
+rm -f package-lock.json *.tgz
+
+echo "-- Install dependencies and create package-lock"
+# If you have npm production dependencies, uncomment the following line
+npm install --production --package-lock
+
+echo "-- Generate SHA256 checksums"
+shasum --algorithm 256 manifest.json package.json index.js lib/*.js LICENSE README.md > SHA256SUMS
+rm -rf node_modules/.bin
+
+# If you have npm production dependencies, uncomment the following line
+find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+
+echo "-- Pack to tar archive"
+TARFILE=`npm pack`
+
+echo "-- Add dependent node_modules to archive"
+# If you have npm production dependencies, uncomment the following 3 lines
+tar --extract --ungzip --file=${TARFILE}
+cp -r node_modules ./package
+tar --create --gzip --file=${TARFILE} ./package
+
+echo "-- Show SHA265 checksum of package"
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sums
+
+rm -rf SHA256SUMS package
