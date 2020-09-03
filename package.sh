f38028a0e1acce178e09bd65edf1e4f33ffd3bf1
bvmensvoort <info@bvmensvoort.nl>
2020-09-03 20:07:23 +0200

    Update GitHub flow

--- package.sh
+++ package.sh
@@ -8,8 +8,7 @@ echo "-- Install dependencies and create package-lock"
 npm install --production --package-lock
 
 echo "-- Generate SHA256 checksums"
-shasum --algorithm 256 manifest.json package.json index.js lib/*.js LICENSE README.md > SHA256SUMS
-rm -rf node_modules/.bin
+shasum --algorithm 256 package.json examples/* philips-air/* LICENSE README.md > SHA256SUMS
 
 # If you have npm production dependencies, uncomment the following line
 find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
